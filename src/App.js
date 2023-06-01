b2ba74a3fb406053bd2a371f02427a7092ae77ed
MOHAMAD ALMEARI <mohamet.almeari@gmail.com>
2023-06-01 06:23:06 +0000

    Create logic dir & files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Calculator from './components/Calculator';
 import './App.css';
+import calculate from './logic/calculate';
 
 function App() {
   return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,36 @@
 import PropTypes from 'prop-types';
 import './Calculator.css';
+import { useState } from 'react';
 
-const Calculator = ({ result }) => (
+const Calculator = ({ result }) => {
+  return (
   <div className="container">
     <p className="result">
       {result}
     </p>
-    <div className="calc-pad">
-      <span className="AC">AC</span>
-      <span className="sign">+/-</span>
-      <span className="percent">%</span>
-      <span className="operator division">÷</span>
-      <span className="num num7">7</span>
-      <span className="num num8">8</span>
-      <span className="num num9">9</span>
-      <span className="operator multiplication">×</span>
-      <span className="num num4">4</span>
-      <span className="num num5">5</span>
-      <span className="num num6">6</span>
-      <span className="operator subtraction">-</span>
-      <span className="num num1">1</span>
-      <span className="num num2">2</span>
-      <span className="num num3">3</span>
-      <span className="operator Addition">+</span>
-      <span className="num num0">0</span>
-      <span className="num decimal">.</span>
-      <span className="operator equal">=</span>
+    <div className="calc-pad" onClick={(e) => {console.log(e.target.value)}}>
+      <button className="AC" value={"AC"}>AC</button>
+      <button className="sign" value={"+/-"}>+/-</button>
+      <button className="percent" value={"%"}>%</button>
+      <button className="operator division" value={"÷"}>÷</button>
+      <button className="num num7" value={"7"}>7</button>
+      <button className="num num8" value={"8"}>8</button>
+      <button className="num num9" value={"9"}>9</button>
+      <button className="operator multiplication" value={"×"}>×</button>
+      <button className="num num4" value={"4"}>4</button>
+      <button className="num num5" value={"5"}>5</button>
+      <button className="num num6" value={"6"}>6</button>
+      <button className="operator subtraction" value={"-"}>-</button>
+      <button className="num num1" value={"1"}>1</button>
+      <button className="num num2" value={"2"}>2</button>
+      <button className="num num3" value={"3"}>3</button>
+      <button className="operator Addition" value={"+"}>+</button>
+      <button className="num num0" value={"0"}>0</button>
+      <button className="num decimal" value={"."}>.</button>
+      <button className="operator equal" value={"="}>=</button>
     </div>
   </div>
-);
+)};
 
 Calculator.propTypes = {
   result: PropTypes.number,
