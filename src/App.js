e1182fd1277c48c5f62804ea8202bba703cde963
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-01 12:03:52 +0300

    Import and apply calculate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Calculator from './components/Calculator';
 import './App.css';
-import calculate from './logic/calculate';
 
 function App() {
   return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,48 @@
-import PropTypes from 'prop-types';
-import './Calculator.css';
 import { useState } from 'react';
+import './Calculator.css';
+import calculate from '../logic/calculate';
 
-const Calculator = ({ result }) => {
+const Calculator = () => {
+  const [result, setResult] = useState({});
+  const calc = (e) => {
+    setResult(calculate(result, e.target.value));
+  };
   return (
-  <div className="container">
-    <p className="result">
-      {result}
-    </p>
-    <div className="calc-pad" onClick={(e) => {console.log(e.target.value)}}>
-      <button className="AC" value={"AC"}>AC</button>
-      <button className="sign" value={"+/-"}>+/-</button>
-      <button className="percent" value={"%"}>%</button>
-      <button className="operator division" value={"÷"}>÷</button>
-      <button className="num num7" value={"7"}>7</button>
-      <button className="num num8" value={"8"}>8</button>
-      <button className="num num9" value={"9"}>9</button>
-      <button className="operator multiplication" value={"×"}>×</button>
-      <button className="num num4" value={"4"}>4</button>
-      <button className="num num5" value={"5"}>5</button>
-      <button className="num num6" value={"6"}>6</button>
-      <button className="operator subtraction" value={"-"}>-</button>
-      <button className="num num1" value={"1"}>1</button>
-      <button className="num num2" value={"2"}>2</button>
-      <button className="num num3" value={"3"}>3</button>
-      <button className="operator Addition" value={"+"}>+</button>
-      <button className="num num0" value={"0"}>0</button>
-      <button className="num decimal" value={"."}>.</button>
-      <button className="operator equal" value={"="}>=</button>
+    <div className="container">
+      <p className="result">
+        {result.next ? result.next : result.total}
+      </p>
+      <div className="calc-pad" role="presentation" onClick={(e) => { calc(e); }}>
+        <button type="button" className="AC" value="AC">AC</button>
+        <button type="button" className="sign" value="+/-">+/-</button>
+        <button type="button" className="percent" value="%">%</button>
+        <button type="button" className="operator division" value="÷">÷</button>
+        <button type="button" className="num num7" value="7">7</button>
+        <button type="button" className="num num8" value="8">8</button>
+        <button type="button" className="num num9" value="9">9</button>
+        <button type="button" className="operator multiplication" value="x">x</button>
+        <button type="button" className="num num4" value="4">4</button>
+        <button type="button" className="num num5" value="5">5</button>
+        <button type="button" className="num num6" value="6">6</button>
+        <button type="button" className="operator subtraction" value="-">-</button>
+        <button type="button" className="num num1" value="1">1</button>
+        <button type="button" className="num num2" value="2">2</button>
+        <button type="button" className="num num3" value="3">3</button>
+        <button type="button" className="operator Addition" value="+">+</button>
+        <button type="button" className="num num0" value="0">0</button>
+        <button type="button" className="num decimal" value=".">.</button>
+        <button type="button" className="operator equal" value="=">=</button>
+      </div>
     </div>
-  </div>
-)};
-
-Calculator.propTypes = {
-  result: PropTypes.number,
+  );
 };
 
-Calculator.defaultProps = {
-  result: 0,
-};
+// Calculator.propTypes = {
+//   result: PropTypes.number,
+// };
+
+// Calculator.defaultProps = {
+//   result: 0,
+// };
 
 export default Calculator;
