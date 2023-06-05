597d4b29833cdd478d4228e498174255b6179dab
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-05 16:13:06 +0300

    Add routes & Home Page

---
+++ src/routes/HomePage.js
@@ -0,0 +1,16 @@
+const HomePage = () => (
+  <div>
+    <h2>
+      Welcome to our page!
+    </h2>
+    <p>
+      This website is a simple calculator that can perform basic math
+      operations, including addition, subtraction, multiplication,
+      division, and modulo. Additionally, the website features a
+      page dedicated to cool random quotes that aim to keep you
+      motivated and optimistic. Feel free to check it out!
+
+    </p>
+  </div>
+);
+export default HomePage;
