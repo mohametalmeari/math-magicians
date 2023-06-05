084b76f3165680f5bea83ecd8d84db80de27adf8
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-05 23:47:30 +0300

    Test Calculator Component

---
+++ src/components/Calculator.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Calculator from './Calculator';
+
+describe('Calculator Component', () => {
+  test('test render of calculator page', () => {
+    const { container } = render(<Calculator />);
+    expect(container).toMatchSnapshot();
+  });
+});
